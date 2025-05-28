d7873f1117d5eae7250eb8bf48c39a3c69c61e99
kiu314Shota <tsertsvadze.shota@kiu.edu.ge>
2025-05-29 01:02:31 +0400

    Delete kiux/src/main/java/com/kiux/backend directory

--- kiux/src/main/java/com/kiux/backend/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kiux.backend.repository;
-import com.kiux.backend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
