26f7156cae66698a3f0380055a28d07c66232e4c
kiu314Shota <tsertsvadze.shota@kiu.edu.ge>
2025-05-29 01:02:31 +0400

    Delete kiux/src/main/java/com/kiux/backend directory

--- kiux/src/main/java/com/kiux/backend/repository/HubRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kiux.backend.repository;
-
-import com.kiux.backend.model.Hub;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-@Repository
-public interface HubRepository extends JpaRepository<Hub, Long> {}
