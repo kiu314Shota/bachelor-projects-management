00c8f7823525af46b8c5911a5c3f3724446f65b7
kiu314Shota <tsertsvadze.shota@kiu.edu.ge>
2025-05-29 01:01:45 +0400

    Add files via upload
    added roles, verification, security, tests

---
+++ kiux/src/main/java/com/kiu/kiuxkiu/KiuXKiuApplication.java
@@ -0,0 +1,13 @@
+package com.kiu.kiuxkiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiuXKiuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiuXKiuApplication.class, args);
+    }
+
+}
