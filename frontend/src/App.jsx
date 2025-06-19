5d82c84076c9c714aa7ce9d6eae34fd12c00881e
SabaBeradze <113034681+SabaBeradze@users.noreply.github.com>
2025-06-20 02:50:44 +0400

    mc

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CreateUserPage from "./pages/register-page/CreateUserPage.jsx";
 import HomePage from "./pages/home-page/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import PostsPage from "./pages/PostPage.jsx";
 
 export default function App() {
     return (
@@ -8,6 +10,8 @@ export default function App() {
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/register" element={<CreateUserPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/posts" element={<PostsPage />} />
             </Routes>
         </BrowserRouter>
     );
